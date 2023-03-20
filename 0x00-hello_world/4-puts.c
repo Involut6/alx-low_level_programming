151366309154fd823877c7ae8573924952a0890b
involut6 <olamide@gmail.com>
2023-03-20 01:40:02 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,6 @@
 /*
  * main - Entry point
  *
- * Description: 'Function returns text using puts'
  *
  * Return: Always 0 (Success)
  */
@@ -11,7 +10,7 @@
 int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
