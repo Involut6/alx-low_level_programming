34e0131fb0a217d75c5c6e3f03e9580553670d5a
involut6 <olamide@gmail.com>
2023-03-19 22:04:32 -0700

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ *
+ * Description: 'Function returns text using puts'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	
+
 	puts("\"Programming is like building a multilingual puzzle\n");
-	
+
 	return (0);
 }
