69bfb30635a4a0c70ac279fee91880fd2a5f5a2f
involut6 <olamide@gmail.com>
2023-03-21 15:17:19 -0700

    0x02-alphbet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints lowercase alphabets using _putchar
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
