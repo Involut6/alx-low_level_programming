3192684f7deee8bee9e6131ebfb8333bd3e20753
involut6 <olamide@gmail.com>
2023-03-23 09:34:30 -0700

    remainder

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,14 +10,5 @@ int add(int a, int b)
 {
 	int c = a + b;
 
-	if (c >= 10)
-	{
-		_putchar(c / 10 + '0');
-		_putchar(c % 10 + '0');
-	}
-	else
-	{
-		_putchar(c + '0');
-	}
-	return (0);
+	return (c);
 }
